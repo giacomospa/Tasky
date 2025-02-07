9c204264e8504c0115f8f2c707485d1fb8a671d1
Giacomo Spadafina <giacomospa69@gmail.com>
2025-02-07 16:53:17 +0100

    Stabilito Tema e Font

---
+++ resources/docs/readme.md
@@ -0,0 +1,12 @@
+## Info dev progetto
+
+# Colors e Font:
+
+- Primario: 36, 35, 49;
+- Secondario: 244, 246, 245;
+- Accento1: 229, 99, 153;
+- Accento2: 20, 148, 184;
+
+- Font-Primario: "Orbitron", serif;
+- Font-Secondario: "Montserrat", serif;
+
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
     @vite(['resources/css/app.css','resources/css/style.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
+    <title>Document</title>
 </head>
 <body>
     {{$slot}}
 </body>
 </html>
+
